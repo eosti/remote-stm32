6241c2d0c7687d539c68170239d28c0e9f90c4b1
eosti <reidsox1@gmail.com>
2021-01-15 22:51:52 -0800

    fix variable declarations

--- install.sh
+++ install.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-UDEV_RULES_LOCATION = "/etc/udev/rules.d"
-UDEV_RULE           = "40-stlink.rules"
-SCRIPT_LOCATION     = "/usr/local/bin"
-SCRIPT              = "udev-stlink.sh"
+UDEV_RULES_LOCATION="/etc/udev/rules.d"
+UDEV_RULE="40-stlink.rules"
+SCRIPT_LOCATION="/usr/local/bin"
+SCRIPT="udev-stlink.sh"
 
 # Check if running as root
 if [ "$EUID" -ne 0 ]
-  then printf "Please run as root."
+  then printf "Please run as root.\n"
   exit 1; 
 fi
 
 # Make sure prerequisites are installed, if not, install them
 if [ ! dpkg -l stlink &> /dev/null ]; then
-    printf "Package stlink does not exist, please install to continue!"
+    printf "Package stlink does not exist, please install to continue!\n"
     exit 1;
 fi
 
 if [ ! dpkg -l at &> /dev/null ]; then
-    printf "Package at does not exist, please install to continue!"
+    printf "Package at does not exist, please install to continue!\n"
     exit 1;
 fi
 
 # Make sure that the file locations exist
 if [ ! -d $SCRIPT_LOCATION ]; then
-    printf "$SCRIPT_LOCATION does not exists, creating directory..."
+    printf "$SCRIPT_LOCATION does not exists, creating directory...\n"
     mkdir -p -m 755 $SCRIPT_LOCATION > /dev/null
 fi
 
 if [ ! -d $UDEV_LOCATION ]; then
-    printf "$UDEV_LOCATION does not exist, creating directory..."
+    printf "$UDEV_LOCATION does not exist, creating directory...\n"
     mkdir -p -m 755 $UDEV_RULES_LOCATION > /dev/null
 fi
 
 # Verify that this directory has the correct files to copy
-if [ ! \( -f $UDEV_RULE -a -f $SCRIPT\)]; then
-    printf "Incorrect run location: files to copy not found."
+if [ ! \( -f $UDEV_RULE -a -f $SCRIPT \) ]; then
+    printf "Incorrect run location: files to copy not found.\n"
     exit 1;
 fi
 
@@ -48,8 +48,8 @@ printf "Copying $UDEV_RULE to $UDEV_RULES_LOCATION/$UDEV_RULE..."
 cp $UDEV_RULE $UDEV_RULES_LOCATION/$UDEV_RULE > /dev/null
 
 # Finish up
-printf "Restarting udev"
+printf "Restarting udev\n"
 udevadm control --reload-rules && udevadm trigger
 
-printf "All done!"
+printf "All done!\n"
 exit 0;
